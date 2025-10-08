2ba993229c064f922d900f861c54ca00153d6e06
Akshaykumar110904 <ravulakariakshaykumar@gmail.com>
2025-10-08 09:47:46 +0530

    Started React Project and added Login Page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Login from './components/Login.jsx'
+
+const App = () => {
+
+  return (
+    <>
+      <div className='flex flex-col min-h-screen justify-center items-center bg-[#21427C]'>
+        <Login />
+
+      </div>
+    </>
+  )
+}
+
+export default App
